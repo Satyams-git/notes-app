a206470cf30429d46c38f53d0f5ee488e43f6ff4
Satyams-git <satyam.du.in@gmail.com>
2026-04-25 20:34:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Qualibytes - Notes App</title>
+    <title>Satyam - Notes App</title>
     <style>
         body {
             font-family: "Segoe UI", Arial, sans-serif;
